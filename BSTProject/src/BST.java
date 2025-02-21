e188c32cff51245f7cfb11a773b4351686299ae5
logantormakim <ltormakim@gmail.com>
2025-02-21 15:36:44 +0000

    2/21 priogness

--- BSTProject/src/BST.java
+++ BSTProject/src/BST.java
@@ -2,6 +2,39 @@ import java.lang.Comparable;
 
 public class BST implements BSTInterface
 {
-    private TreeNode root;
+    private TreeNode root = new TreeNode(null);
+
+    public BST () {
+        root=null;
+    }
     
-}
+    public void add(Comparable val) {
+        if(root = null)
+	            root = new TreeNode(val, null,null);
+        else if (val.compareTo(root.getValue())) {
+	        addHelper(val, root.getLeft(), root);
+        }
+        else {
+	        addHelper(val, root.getRight(), root);
+        }
+    }
+        private void addHelper(Comparable val, TreeNode child, TreeNode parent)
+        {
+            if (child == null) {
+	            if (parent.getValue().compareTo(val) >=0) {
+			            parent.setLeft(new TreeNode(val));
+                }
+		    else {
+			parent.setRight(new TreeNode(val));
+            }
+            }
+	        else if (val.compareTo(child.getValue()) <0) {
+                addHelper(val, child.getLeft(), child);
+            }
+	        else {
+                addHelper(val, child.getRight(), child);
+            }
+        }
+        
+        
+    }   
